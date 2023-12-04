3aba45e9e102031f75567eb23afcc259df338a81
AliErenCeylan <152271409+AliErenCeylan@users.noreply.github.com>
2023-12-04 19:59:24 +0100

    Add files via upload

---
+++ schere_stein_papier_echse_spock/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("Benutzernamen Eingeben: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Spieler: " + name);
+        Console.WriteLine("          ");
+        Console.ResetColor();
+
+        while (0 == 0)
+        {
+
+            Console.Write("Spielzug Eingeben: ");
+            string Spieler = Console.ReadLine();
+            Console.WriteLine(name + "'s Spielzug: " +Spieler);
+
+            int[] Moves = new int[]
+            {
+            1,//Schere
+            2,//Stein
+            3,//Papier
+            4,//Echse
+            5,//Spock
+            };
+
+
+            if (Spieler != "Schere" && Spieler != "Stein" && Spieler != "Papier" && Spieler != "Echse" && Spieler != "Spock")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ungültige Eingabe");
+                Console.ResetColor ();
+            }
+            else
+            {
+                Random random = new Random();
+                int index = random.Next(Moves.Length);
+                int Bot = Moves[index];
+
+                if (Bot == 1)
+                {
+                    Console.WriteLine("Computer's Zug: Schere");
+                }
+                else if (Bot == 2)
+                {
+                    Console.WriteLine("Computer's Zug: Stein");
+                }
+                else if (Bot == 3)
+                {
+                    Console.WriteLine("Computer's Zug: Papier");
+                }
+                else if (Bot == 4)
+                {
+                    Console.WriteLine("Computer's Zug: Echse");
+                }
+                else if (Bot == 5)
+                {
+                    Console.WriteLine("Computer's Zug: Spock");
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                if (Spieler == "Schere" && Bot == 3)
+                {
+                    Console.WriteLine("Schere schneidet Papier. Du hast Gewonnen");
+                }
+                else if (Spieler == "Papier" && Bot == 2)
+                {
+                    Console.WriteLine("Papier bedeckt Stein. Du hast Gewonnen");
+                }
+                else if (Spieler == "Stein" && Bot == 4)
+                {
+                    Console.WriteLine("Stein zerquetscht Echse. Du hast Gewonnen");
+                }
+                else if (Spieler == "Echse" && Bot == 5)
+                {
+                    Console.WriteLine("Echse vergiftet Spock. Du hast Gewonnen");
+                }
+                else if (Spieler == "Spock" && Bot == 1)
+                {
+                    Console.WriteLine("Spock zertrümmert Schere. Du hast Gewonnen");
+                }
+                else if (Spieler == "Schere" && Bot == 4)
+                {
+                    Console.WriteLine("Schere köpft Echse. Du hast Gewonnen");
+                }
+                else if (Spieler == "Echse" && Bot == 3)
+                {
+                    Console.WriteLine("Echse frisst Papier. Du hast Gewonnen");
+                }
+                else if (Spieler == "Papier" && Bot == 5)
+                {
+                    Console.WriteLine("Papier widerlegt Spock. Du hast Gewonnen");
+                }
+                else if (Spieler == "Spock" && Bot == 2)
+                {
+                    Console.WriteLine("Spock verdampft Stein. Du hast Gewonnen");
+                }
+                else if (Spieler == "Stein" && Bot == 1)
+                {
+                    Console.WriteLine("Stein zertrümmert Schere. Du hast Gewonnen");
+                }
+
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                if (Spieler == "Papier" && Bot == 1)
+                {
+                    Console.WriteLine("Schere schneidet Papier. Du hast Verloren");
+                }
+                else if (Spieler == "Stein" && Bot == 3)
+                {
+                    Console.WriteLine("Papier bedeckt Stein. Du hast Verloren");
+                }
+                else if (Spieler == "Echse" && Bot == 2)
+                {
+                    Console.WriteLine("Stein zerquetscht Echse. Du hast Verloren");
+                }
+                else if (Spieler == "Spock" && Bot == 4)
+                {
+                    Console.WriteLine("Echse vergiftet Spock. Du hast Verloren");
+                }
+                else if (Spieler == "Schere" && Bot == 5)
+                {
+                    Console.WriteLine("Spock zertrümmert Schere. Du hast Verloren");
+                }
+                else if (Spieler == "Echse" && Bot == 1)
+                {
+                    Console.WriteLine("Schere köpft Echse. Du hast Verloren");
+                }
+                else if (Spieler == "Papier" && Bot == 4)
+                {
+                    Console.WriteLine("Echse frisst Papier. Du hast Verloren");
+                }
+                else if (Spieler == "Spock" && Bot == 3)
+                {
+                    Console.WriteLine("Papier widerlegt Spock. Du hast Verloren");
+                }
+                else if (Spieler == "Stein" && Bot == 5)
+                {
+                    Console.WriteLine("Spock verdampft Stein. Du hast Verloren");
+                }
+                else if (Spieler == "Schere" && Bot == 2)
+                {
+                    Console.WriteLine("Stein zertrümmert Schere. Du hast Verloren");
+                }
+
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+
+                if (Spieler == "Schere" && Bot == 1 || Spieler == "Stein" && Bot == 2 || Spieler == "Papier" && Bot == 3 || Spieler == "Echse" && Bot == 4 || Spieler == "Spock" && Bot == 5)
+                {
+                    Console.WriteLine("Unentschieden");
+                }
+
+                Console.ResetColor();
+            }
+            Console.WriteLine("             ");
+        }
+        Console.ReadKey();
+    }
+}
---
+++ schere_stein_papier_echse_spock/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("schere_stein_papier_echse_spock")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("schere_stein_papier_echse_spock")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cea37dc0-4a96-4952-ac09-ddc344d407e9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
